e2ef7d3c5525266b93398cedd7142e919ce3c90b
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-12 13:19:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, newName) {
+  katzDeli.push(newName);
+  return 'Welcome, ' + newName + '. You are number ' + (katzDeli[newName] + 1) + ' in line.'
+}
+
+function nowServing(katzDeliLine) {
+
+  while (katzDeliLine.length > 0) {
+    return katzDeliLine[0]
+  } if (katzDeliLine.length <= 0) {
+    return 'There is nobody waiting to be served!'
+  }
+}
+
+function currentLine(katzDeliLine) {
+  let counter = 0
+  if (counter <= katzDeliLine.length - 1) {
+    while (counter <= katzDeliLine.length - 1) {
+      if (counter === 0) {
+        katzDeliLine[counter] = (counter + 1) + '. ' + katzDeliLine[counter];
+        counter++;
+      } else {
+      katzDeliLine[counter] = ' ' + (counter + 1) + '. ' + katzDeliLine[counter];
+        counter++;
+      }
+    } return 'The line is currently: ' + katzDeliLine;
+  } else {
+    return "The line is currently empty."
+  }
+}
